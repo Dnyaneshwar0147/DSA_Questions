73b2244218e2ce6b18f6c03549ec354b58f71985
Dnyaneshwar Danane <dananednyaneshwar10@gmail.com>
2025-07-18 16:31:20 +0530

    firstcommit

---
+++ Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[]args){
+        int [] num = {2,7,11,15};
+        int sum = num[0] + num[1];
+        System.out.println("sum: " + sum);
+
+        
+    }
+}
